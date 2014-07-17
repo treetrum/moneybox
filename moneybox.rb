3334608bea6917a6520aacbf7b3b8db44be823f7
Sam <samjd1993@gmail.com>
2014-07-17 10:44:02 +1000

    Add better code documentation

--- moneybox.rb
+++ moneybox.rb
@@ -1,26 +1,28 @@
+# Variables
 $filename = "data"
 $balance = nil
 
-# ------- #
-# Methods #
-# ------- #
+# Methods 
 def refresh_screen()
 	system "clear" or system "cls"
 end
-
 def add
 	puts "How much would you like to add? "
 	amt = gets.chomp.to_f
 	$balance += amt
 	puts "Succesfully added $#{amt.round(2)} to your balance.\nYour new balance is $#{$balance.round(2)}"
 end
-
 def subtract
 	puts "How much would you like to subtract? "
 	amt = gets.chomp.to_f
 	$balance -= amt
 	puts "Succesfully subtracted $#{amt.round(2)} to your balance.\nYour new balance is $#{$balance.round(2)}"
 end
+
+
+# ------------------------------------------------ #
+# ----------------- MAIN PROGRAM ----------------- #
+# ------------------------------------------------ #
 
 # If $filename does not exist create it and initialise it with $0.00
 if !File.exist?($filename)
@@ -35,16 +37,15 @@ puts "You have $#{$balance.round(2)}"
 # Ask if the user would like to add or subtract balance? or Save & Quit.
 response = nil
 
+# Main program loop
 puts "Would you like to Add or Subtract (a/s) or quit? (q)"
 response = gets.chomp.to_s
-
 while response != 'q'
 	case response
 		when "a" then add
 		when "s" then subtract
 		else puts "Error, that is not a valid response."
 	end	
-
 	puts "Would you like to Add or Subtract (a/s) or quit? (q)"
 	response = gets.chomp.to_s
 end
