ab285764af2f8ead883a6678fbd01d21639cba6f
Sam <samjd1993@gmail.com>
2014-07-17 10:39:18 +1000

    Initial Alpha

--- moneybox.rb
+++ moneybox.rb
@@ -1,5 +1,5 @@
-$filename = "data.txt"
-$balance = -1
+$filename = "data"
+$balance = nil
 
 # ------- #
 # Methods #
@@ -12,14 +12,14 @@ def add
 	puts "How much would you like to add? "
 	amt = gets.chomp.to_f
 	$balance += amt
-	puts "Succesfully added $#{amt.round(2)} to your balance.\nYour new balance is $#{balance.round(2)}"
+	puts "Succesfully added $#{amt.round(2)} to your balance.\nYour new balance is $#{$balance.round(2)}"
 end
 
 def subtract
 	puts "How much would you like to subtract? "
 	amt = gets.chomp.to_f
-	$balance += amt
-	puts "Succesfully subtracted $#{amt.round(2)} to your balance.\nYour new balance is $#{balance.round(2)}"
+	$balance -= amt
+	puts "Succesfully subtracted $#{amt.round(2)} to your balance.\nYour new balance is $#{$balance.round(2)}"
 end
 
 # If $filename does not exist create it and initialise it with $0.00
@@ -29,18 +29,30 @@ end
 
 # Read and display from file
 refresh_screen
-file = File.open($filename, "r") {|file| $balance = file.gets.to_f }
+File.open($filename, "r") {|file| $balance = file.gets.to_f }
 puts "You have $#{$balance.round(2)}"
 
 # Ask if the user would like to add or subtract balance? or Save & Quit.
 response = nil
 
+puts "Would you like to Add or Subtract (a/s) or quit? (q)"
+response = gets.chomp.to_s
+
 while response != 'q'
+	case response
+		when "a" then add
+		when "s" then subtract
+		else puts "Error, that is not a valid response."
+	end	
+
 	puts "Would you like to Add or Subtract (a/s) or quit? (q)"
 	response = gets.chomp.to_s
-	if response == "a" {add}
-	elsif response == "s" {subtract}
-	else {puts "Error, that is not a valid response."}
 end
 
 # Save and quit
+if response = "q"
+	File.open($filename, "w") { |file| file.write($balance)  }
+	refresh_screen
+	puts "Succesfully saved to file."
+	puts "Balance = $#{$balance.round(2)}"
+end
